8631a7b5d2491a186ac6d6226edf30dacfb239eb
seokrae <kslbsh@gmail.com>
2022-05-24 23:21:06 +0900

    test: id 값 없이 객체 테스트를 어떻게 하지

--- domain-test/src/test/java/com/example/transactiontest/application/order/domain/OrderTest.java
+++ domain-test/src/test/java/com/example/transactiontest/application/order/domain/OrderTest.java
@@ -26,12 +26,12 @@ class OrderTest {
 		member = makeMember(1L, address("서울시", "논현로", "11길"));
 		album = album(2L, 1000, 10, "앨범명", "아티스트", "팝");
 		book = book(3L, 500, 5, "서적명", "저자", "ISBN");
-		delivery = new Delivery(member.getAddress());
+		delivery = new Delivery(7L, member.getAddress());
 	}
 
 	@Nested
 	@DisplayName("주문 등록 관련")
-	class CreateOrderTest {
+	class OrderAndCancelTest {
 
 		@DisplayName("주문 등록 테스트")
 		@Test
@@ -44,20 +44,15 @@ class OrderTest {
 			int totalPrice = java.util.Arrays.stream(array)
 					.mapToInt(OrderItem::getTotalPrice)
 					.sum();
-			Order order = Order.createOrder(member, delivery, array);
+			Order order = Order.createOrder(6L, member, delivery, array);
 
 			assertThat(order.getMember()).isEqualTo(member);
 			assertThat(order.getOrderItems()).contains(array);
 			assertThat(order.getDelivery()).isEqualTo(delivery);
 			assertThat(order.getTotalPrice()).isEqualTo(totalPrice);
 		}
-	}
 
-	@Nested
-	@DisplayName("주문 등록 및 취소 상태 확인")
-	class OrderAndCancelTest {
-
-		@DisplayName("주문 최종 금액 조회 테스트")
+		@DisplayName("주문 취소 상태 변경(ORDER -> CANCEL) 테스트")
 		@Test
 		void testCase2() {
 			OrderItem[] array = Arrays.array(
@@ -65,8 +60,10 @@ class OrderTest {
 					, OrderItem.createOrderItem(5L, book, book.getPrice(), 4)
 			);
 
-			Order order = Order.createOrder(member, delivery, array);
+			Order order = Order.createOrder(6L, member, delivery, array);
+			Delivery delivery = order.getDelivery();
 			assertThat(order.getStatus()).isEqualTo(OrderStatus.ORDER);
+			assertThat(delivery.getStatus()).isEqualTo(DeliveryStatus.READY);
 
 			order.cancel();
 			assertThat(order.getStatus()).isEqualTo(OrderStatus.CANCEL);
---
+++ domain-test/src/test/java/com/example/transactiontest/application/order/repository/OrderRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.example.transactiontest.application.order.repository;
+
+import com.example.transactiontest.application.member.domain.Member;
+import com.example.transactiontest.application.order.domain.*;
+import com.example.transactiontest.application.product.domain.Item;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static com.example.transactiontest.application.product.stub.ItemStub.album;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Order 클래스")
+@DataJpaTest
+class OrderRepositoryTest {
+
+	@Autowired
+	protected OrderRepository orderRepository;
+	private Order orderEntity;
+	private OrderItem orderItem;
+	private Delivery delivery;
+	private Member member;
+
+	@Nested
+	@DisplayName("주문 등록 관련")
+	class OrderTest {
+
+		@BeforeEach
+		void setUp() {
+			member = new Member.Builder(3L).build();
+			delivery = new Delivery(7L, member.getAddress());
+			Item item = album(2L, 1000, 5, "앨범", "아티스트", "팜");
+			orderItem = OrderItem.createOrderItem(1L, item, item.getPrice(), 2);
+			orderEntity = Order.createOrder(4L, member, delivery, orderItem);
+		}
+
+		@DisplayName("주문 등록 시 주문상태, 배달상태 정보 확인 테스트")
+		@Test
+		void testCase1() {
+			Order order = orderRepository.save(orderEntity);
+			Delivery orderDelivery = order.getDelivery();
+
+			assertThat(order.getStatus()).isEqualTo(OrderStatus.ORDER);
+			assertThat(orderDelivery.getStatus()).isEqualTo(DeliveryStatus.READY);
+		}
+
+		@DisplayName("주문 등록 및 취소 시 상태 정보 확인 테스트")
+		@Test
+		void testCase2() {
+			Order order = Order.createOrder(5L, member, delivery, orderItem);
+			orderRepository.save(order);
+			Delivery orderDelivery = order.getDelivery();
+
+			assertThat(order.getStatus()).isEqualTo(OrderStatus.ORDER);
+			assertThat(orderDelivery.getStatus()).isEqualTo(DeliveryStatus.READY);
+
+			order.cancel();
+
+			assertThat(order.getStatus()).isEqualTo(OrderStatus.CANCEL);
+			assertThat(order.getDelivery().getStatus()).isEqualTo(DeliveryStatus.READY);
+		}
+	}
+}
